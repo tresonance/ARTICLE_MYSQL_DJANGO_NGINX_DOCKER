ab724a80f58f33d34abb3c4737450a73567b36e1
Ibrahima Traore <tresonance2@gmail.com>
2023-08-19 20:54:34 +0200

    clean workspaxe

--- sh/create_superuser_dev.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "from django.contrib.auth.models import User; User.objects.create_superuser($DJANGO_SUPERUSER_USERNAME,$DJANGO_SUPERUSER_EMAIL,$DJANGO_SUPERUSER_PASSWORD)" | python /usr/src/app/myproject/manage.py shell 
